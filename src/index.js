ad8691dae79cc0fc59f78b90128abb056784a344
Patrick Kennefod Annan <100440561+Kenneford@users.noreply.github.com>
2022-08-05 23:53:50 +0200

    Revert "Router updated"
    This reverts commit 0f412a96435b01978b3d7457cd5b1cc4e87fd1ed.

--- src/index.js
+++ src/index.js
@@ -9,16 +9,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <Router>
-            <Routes>
-                <Route path='/' element={<App />} />
-                <Route path='about' element={<About />} />
-                <Route path='services' element={<Services />} />
-                <Route path='contact' element={<Contact />} />
-            </Routes>
-        </Router>
+  <Router>
+      <React.StrictMode>
+        <App />
       </React.StrictMode>
+      <Routes>
+        <Route path='/about' element={<About />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+  </Router>
 );
 
 
