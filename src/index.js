aea982a311370abaf0cff070c2edf2c733650313
Patrick Kennefod Annan <100440561+Kenneford@users.noreply.github.com>
2022-08-05 23:12:14 +0200

    Router issue fixed

--- src/index.js
+++ src/index.js
@@ -10,11 +10,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
       <Routes>
-            <Route path='/' element={<App />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/services' element={<Services />} />
-            <Route path='/contact' element={<Contact />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
   </Router>
 );
